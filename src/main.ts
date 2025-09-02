7e35b5c69b7513cccb85896cabdb41c1ba065b20
jeeva-haiva-ai <jeeva@haiva.ai>
2025-09-02 17:35:38 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ import { environment } from './environments/environment';
 if (environment.production) {
   enableProdMode();
 }
+// comment for testing agent
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.log(err));
